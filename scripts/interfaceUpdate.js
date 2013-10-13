79aee19da99befb6c35a54b641d80a57aac2516c
Nihcolas La Roux <larouxn@gmail.com>
2013-10-13 15:52:36 -0400

    Fixed animate.css not linking correctly and setup bottom bar buttons

--- scripts/interfaceUpdate.js
+++ scripts/interfaceUpdate.js
@@ -1,4 +1,4 @@
-function updateContents(message) {
+function updateMessages(message) {
   // Get a reference to the #textArea element
   var textArea = document.getElementById('textArea');
   // Get a reference to the last update (first child)
@@ -18,15 +18,6 @@ function updateContents(message) {
   textArea.insertBefore(newElement, theFirstChild);
 }
 
-function originalUpdate()
-{
-    // create a p tag and text node
-    var newP = document.createElement("p");
-    var newText = document.createTextNode("Oh hai, I'm dynamically generated.");
-    
-    newP.appendChild(newText); // add text to created p tag
-    
-    // add p tag and text element to list of text updates in textArea
-    var textArea = document.getElementById("field2");
-    document.body.insertBefore(newP, textArea);
+function updateButtons(variable) {
+  blah;
 }
