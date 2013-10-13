3d1dfc7298cda2207d8ac92dafe623dbacdccf07
Nihcolas La Roux <larouxn@gmail.com>
2013-10-13 12:37:36 -0400

    Finished preliminary textual update function

--- scripts/interfaceUpdate.js
+++ scripts/interfaceUpdate.js
@@ -1,3 +1,23 @@
+function updateContents(message) {
+  // Get a reference to the #textArea element
+  var textArea = document.getElementById('textArea');
+  // Get a reference to the last update (first child)
+  var theFirstChild = textArea.firstChild;
+
+  // Create a new update element
+  var newElement = document.createElement("p");
+  // Create text for element
+  var text = document.createTextNode(message);
+  // Create full update element with text
+  newElement.appendChild(text);
+
+  // Set class for update element to fadeBlock
+  newElement.setAttribute("class", "fadeBlock");
+
+  // Insert the new element before the first child
+  textArea.insertBefore(newElement, theFirstChild);
+}
+
 function originalUpdate()
 {
     // create a p tag and text node
@@ -9,25 +29,4 @@ function originalUpdate()
     // add p tag and text element to list of text updates in textArea
     var textArea = document.getElementById("field2");
     document.body.insertBefore(newP, textArea);
-}
-
-var currentHTML, newHTML, allHTML, text;
-// var message = "Oh hai, watch out for Sunny Jim";
-
-function updateContents(message) {
-  // create variable for current html of #textArea
-  currentHTML = document.getElementById("textArea");
-  
-  // create variable for new html node
-  newHTML = document.creatElement("p");
-  // create varible for new text for new node
-  text = document.createTextNode(message);
-  // combine html node with text node
-  newHTML.appendChild(text);
-  
-  // create variable to hold all html for #textArea
-  allHTMML = currentHTML + newHTML;
-  
-  
-  // almost works, not tested. allHTML may be a string
 }
