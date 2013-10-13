2df133c99a982a8d0329951b989e0ce9e92ee6a3
robwhitaker <Robert.Whitaker1@marist.edu>
2013-10-12 21:52:41 -0400

    movement is now functional

--- scripts/Movement.js
+++ scripts/Movement.js
@@ -1,66 +1,90 @@
-var px =0, py =0;
-var MapMax =20;
-var r = false;
+var player = new Player();
+var mapMax = 20;
+var currentKey = null;
+var interval = null;
+var stepDuration = 500; //milliseconds between steps
 
-var secpstep = 1*1000 // 1000 is converstion to milliseconds
+setInterval(function() {document.getElementById('container').innerHTML=player.x+","+player.y;},100);
+
 function up()
 {
-if (py<MapMax)
-	py+=1;
-//else
-	//prompt hit wall
+    if(player.y < mapMax)
+    	player.y+=1;
+    //else
+    	//prompt hit wall
 }
+
 function down()
 {
-if (py>0)
-	py-=1;
-//else
-	//prompt hit wall
+    if(player.y > 0)
+    	player.y-=1;
+    //else
+    	//prompt hit wall
 }
+
 function right()
 {
-if (px<MapMax)
-	px+=1;
+    if(player.x < mapMax)
+    	player.x+=1;
 
-//else
-	//prompt hit wall
+    //else
+    	//prompt hit wall
 }
 function left()
 {
-if (px>0)
-	px-=1;
-//else 
-	//prompt hit wall
+    if(player.x > 0)
+    	player.x-=1;
+    //else 
+    	//prompt hit wall
 }
-//combination of keydown for running later
+
 document.addEventListener('keydown', function(event) {
     //first code is arrows, second is wasd
     if (event.keyCode == 37 || event.keyCode == 65) {
-        left();
-
+        if(interval == null || currentKey != event.keyCode) {
+          clearInterval(interval);
+          interval = setInterval(left,stepDuration);
+          currentKey = event.keyCode; 
+        } 
     }
-    else if (event.keyCode == 39|| event.keyCode == 86) {
-     	right();    
+    else if (event.keyCode == 39 || event.keyCode == 86) {
+     	if(interval == null || currentKey != event.keyCode) {
+          clearInterval(interval);
+          interval = setInterval(right,stepDuration);
+          currentKey = event.keyCode; 
+        }    
     }
     else if (event.keyCode == 38|| event.keyCode == 87) {
-     	up(); }
+        if(interval == null || currentKey != event.keyCode) {
+          clearInterval(interval);
+     	  interval = setInterval(up,stepDuration);
+          currentKey = event.keyCode; 
+        }
+     }
     else if (event.keyCode == 40|| event.keyCode == 83) {
-     	down();
+     	if(interval == null || currentKey != event.keyCode) {
+          clearInterval(interval);
+          interval = setInterval(down,stepDuration);
+          currentKey = event.keyCode; 
+        } 
      } 
 }, true);
 
 window.addEventListener('keyup',
     function(event){
+        clearInterval(interval);
+        interval = null;
         if (event.keyCode == 37 || event.keyCode == 65) {
-        	alert(px)
+        	//alert(player.x);
 			}
     	else if (event.keyCode == 39|| event.keyCode == 86) {
-     		alert(px);    
+     		//alert(player.x);    
     		}
     	else if (event.keyCode == 38|| event.keyCode == 87) {
-     		alert(py);
+     		//alert(player.y);
      		}
     	else if (event.keyCode == 40|| event.keyCode == 83) {
-     		alert(py); } 
+     		//alert(player.y); 
+        } 
     },
 false);
